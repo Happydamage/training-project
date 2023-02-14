7d20c58aaa73fe43520ee4c7bf2f6da0b93cb77b
Vadim.T <1hananam1ty@gmail.com>
2023-02-14 14:05:29 +0300

    added themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import {Suspense} from 'react';
 import {Counter} from "./components/Counter";
-import './index.scss';
+import {Route, Routes, Link} from "react-router-dom";
+import './styles/index.scss';
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
 
 const App = (): JSX.Element => {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div className={"app"}>
-            <Counter />
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle theme</button>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
+            <Counter/>
         </div>
     );
 };
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainPage = (): JSX.Element => {
+    return (
+        <div>
+            MainPage
+        </div>
+    );
+};
+
+export default MainPage;
