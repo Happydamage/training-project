a9fdbab826adca2522a8b83b5b1e49df7e898032
Vadim.T <1hananam1ty@gmail.com>
2023-03-07 17:15:08 +0300

    add loader

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import {classNames} from "shared/lib/classNames/classNames";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
-import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
-import {useTranslation} from "react-i18next";
-
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = ({className}: NavbarProps): JSX.Element => {
-    const {t} = useTranslation('navbar');
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation('navbar');
 
     return (
-        <div className={classNames(cls.Navbar, {}, [className] )}>
+        <div className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
-                <AppLink theme={AppLinkTheme.SECONDARY} className={cls.mainLink} to={'/'}>{t('Главная')}</AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>{t('О нас')}</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} className={cls.mainLink} to="/">{t('Главная')}</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">{t('О нас')}</AppLink>
             </div>
         </div>
     );
